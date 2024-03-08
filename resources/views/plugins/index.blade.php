b39b914c7340307563310bba872a0c9f8988539a
Ashraful <ashrafulkarim.dev@gmail.com>
2024-03-08 19:44:26 +0600

    overview video added

--- resources/views/plugins/index.blade.php
+++ resources/views/plugins/index.blade.php
@@ -24,33 +24,6 @@
     @if (auth()->user()->role == 'owner')
         <div class="row">
             @foreach ($plugins as $plugin)
-                {{-- delete modal --}}
-                <div class="modal fade" id="confirmDeleteModal" tabindex="-1" aria-labelledby="confirmDeleteModalLabel"
-                    aria-hidden="true">
-                    <div class="modal-dialog">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="confirmDeleteModalLabel">Confirm Delete</h5>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                    aria-label="Close"></button>
-                            </div>
-                            <div class="modal-body">
-                                Are you sure you want to delete this plugin?
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-                                <form id="deleteForm" action="{{ route('plugin.destroy', $plugin) }}" method="POST">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit" class="btn btn-danger">Delete</button>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                {{-- delete modal --}}
-
-
                 <div class="col-md-4">
                     <div class="card shadow-lg p-3">
                         <h4 class="m-0">{{ $plugin->name }}</h4>
@@ -75,6 +48,32 @@
         <a href="{{ route('plugin.create') }}" class="d-inline-block btn btn-primary">
             Create Plugin
         </a>
+        @foreach ($plugins as $plugin)
+            {{-- delete modal --}}
+            <div class="modal fade" id="confirmDeleteModal" tabindex="-1" aria-labelledby="confirmDeleteModalLabel"
+                aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="confirmDeleteModalLabel">Confirm Delete</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            Are you sure you want to delete this plugin?
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                            <form id="deleteForm" action="{{ route('plugin.destroy', $plugin) }}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger">Delete</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {{-- delete modal --}}
+        @endforeach
     @elseif(auth()->user()->role == 'company')
         <div class="row">
             @foreach ($plugins as $plugin)
